8f27e50080842c1685e8344d29d1403679c78f0b
rodolfo-brandao <rodolfobrandao1995@gmail.com>
2026-02-25 21:27:34 -0300

    chore: fix some typos

--- src/bot.py
+++ src/bot.py
@@ -1,20 +1,22 @@
-"""The bot itself. This module is responsible for centralizing
-all events and commands, besides of communicating with API handlers."""
+"""
+The bot itself. This module is responsible for centralizing all events
+and commands, besides of communicating with the API handler.
+"""
 
 
 import os
 import time
+from typing import List, Optional
 import discord
 from dotenv import load_dotenv
-from typing import List, Optional
 from discord.ext import commands
 from discord.ext.commands import Context
-from handler import MovieHandler, Movie
+from src.models import Movie
+from src.handler import search_movies
 
 
 load_dotenv()
-YTS_API_TOKEN: Optional[str] = os.getenv("YTS_API_TOKEN")
-YTS_API_BASE_URL: Optional[str] = os.getenv("YTS_API_BASE_URL")
+DISCORD_TOKEN: Optional[str] = os.getenv("DISCORD_TOKEN")
 
 
 intents = discord.Intents.all()
@@ -29,10 +31,10 @@ async def on_ready() -> None:
     Logs when the bot is running.
     """
 
-    print(f'{bot.user} is running!')
+    print(f"{bot.user} is running!")
 
 
-@bot.command(help='Shows the bot latency.')
+@bot.command(help="Shows the bot latency.")
 async def ping(ctx: Context) -> None:
     """
     Sends a message with the bot's current latency in milliseconds.
@@ -44,27 +46,28 @@ async def ping(ctx: Context) -> None:
     await ctx.send(f"Pong! :ping_pong:\nThe current latency is {round(bot.latency * 1000)}ms")
 
 
-@bot.command(name='info', help="Custom helper command to display all avaliable commands.")
+@bot.command(name="info", help="Custom helper command to display all available commands.")
 async def display_commands(ctx: Context) -> None:
     """
-    Sends a message showing all the bot's avaliable commands.
+    Sends a message showing all the bot's available commands.
 
     :param ctx: Represents the context in which a command is being invoked under.
     :type ctx: Context
     """
 
-    await ctx.send(embed=build_helper_embed())
+    await ctx.send(embed=_build_info_embed())
 
 
-@bot.command(name='search', help='Searches for movie occurences.')
-async def search_movies(ctx, *args) -> None:
+@bot.command(name="search", help="Searches for movie occurrences.")
+async def search_movies_command(ctx: Context, *args) -> None:
     """
-    Searchs for movie occurences based on the given arguments.
+    Searches for movie occurrences based on the given arguments.
 
     :param ctx: Represents the context in which a command is being invoked under.
     :type ctx: Context
 
-    :param *args: The arguments to be used as query in the handler, such as movie title or IMDb code.
+    :param *args: The arguments to be used as query in the handler,
+    such as movie title or IMDb code.
     :type *args: Any
     """
 
@@ -74,16 +77,16 @@ async def search_movies(ctx, *args) -> None:
         await ctx.send("This term seems too short :thinking:\n"
                        "How about searching using an IMDb code or a longer movie title?")
     else:
-        movie_handler = MovieHandler(base_url=YTS_API_BASE_URL or "")
-        api_result = movie_handler.search_movies(term=term)
+        api_result = search_movies(term=term)
 
         if api_result.status_code != 200:
             await ctx.send(api_result.error_message)
-        elif api_result.status_code == 200 and len(api_result.response) == 0:
-            await ctx.send("Sorry. I couldn't find any movie with that title or IMDb code :confused:")
+        elif api_result.status_code == 200 and not api_result.response:
+            await ctx.send(
+                content="Sorry. I couldn't find any movie with that title or IMDb code :confused:"
+            )
         else:
-            embeds = build_search_embeds(movies=api_result.response)
-
+            embeds = _build_search_embeds(movies=api_result.response)  # type: ignore
             for embed in embeds[:10]:
                 await ctx.send(embed=embed)
                 time.sleep(1)
@@ -94,15 +97,15 @@ async def search_movies(ctx, *args) -> None:
 def run() -> None:
     """
     Simply runs the bot.
-    This function should be called in the "main.py" file.
+    This function should be called in the `main.py` file.
     """
 
-    bot.run(token=YTS_API_TOKEN or "")
+    bot.run(token=DISCORD_TOKEN or "")
 
 
-def build_search_embeds(movies: List[Movie]) -> List[discord.Embed]:
+def _build_search_embeds(movies: List[Movie]) -> List[discord.Embed]:
     """
-    Builds the discord embed objects for the "search" command.
+    Builds the discord embed objects for the `?search` command.
 
     :param movies: The list containing all movies found in the search.
     :type movies: List[Movie]
@@ -133,29 +136,29 @@ def build_search_embeds(movies: List[Movie]) -> List[discord.Embed]:
     return embeds
 
 
-def build_helper_embed() -> discord.Embed:
+def _build_info_embed() -> discord.Embed:
     """
-    Builds the discord embed object for the "help" command.
+    Builds the discord embed object for the `?info` command.
 
     :return: A single discord embed object.
     :rtype: Embed
     """
 
     embed = discord.Embed(
-        title='Commands',
+        title="Commands",
         description="These are all the commands I can run :point_down:",
         color=0xFF7F50  # coral
     )
 
     embed.add_field(
-        name='?ping',
-        value='Shows the current latency of the BOT.',
+        name="?ping",
+        value="Shows the current latency of the BOT.",
         inline=False
     )
 
     embed.add_field(
-        name='?search <movie_title> | <imdb_code>',
-        value='Searches for movie occurences (max 10) based on the given title or IMDb code.',
+        name="?search <movie_title> | <imdb_code>",
+        value="Searches for movie occurrences (max 10) based on the given title or IMDb code.",
         inline=False
     )
 
